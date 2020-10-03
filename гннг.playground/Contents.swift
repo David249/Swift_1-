f05c37cf8b21920adf1a5c5daa6b3987680c5405
dat <lestimyr@mail.ru>
2020-10-03 20:59:49 +0300

    // vse na odnom liste

---
+++ гннг.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+var vklad = 100000
+var proc = 6
+var each = 5
+
+for each in 0...5{
+  var income1 = (vklad*proc)/100
+    var income = income1 + ((vklad * proc)*each)/100
+    var profit  = vklad + income
+    print(profit)
+    
+}
+
+
+
+
